3d881cb163247247562cfa996e7046f4845a5769
Shardul Dave <daveshardul@rocketmail.com>
2019-04-23 16:04:30 -0700

    Login

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -2,7 +2,7 @@
 
 #Backend code using Flask framework
 
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
 
 from sqlalchemy import create_engine  
 from sqlalchemy import Column, String, ForeignKey, Integer  
@@ -10,7 +10,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from database_setup import Base, User, Category, Item
 
+from flask import session as login_session
+
+from flask import make_response
+
+import string, random, json 
+import httplib2
+
 app = Flask(__name__)
+
+#Connect to Database and create database session
 
 engine = create_engine("postgres:///catalogdb")  
 Base.metadata.bind = engine
@@ -18,7 +27,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-# JSON APIs Endpoints to view Categories and the items within each categories
+#JSON APIs Endpoints to view Categories and the items within each categories
 
 @app.route('/category/<int:category_id>/item/JSON')
 def categoryItemJSON(category_id):
@@ -33,7 +42,106 @@ def itemJSON(category_id, item_id):
 @app.route('/category/JSON')
 def categoryJSON():
     categories = session.query(Category).all()
-    return jsonify(categories=[r.serialize for r in categories])
+    return jsonify(categories=[c.serialize for c in categories])
+
+#Oauth login for facebook 
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = request.data
+    print ("access token received %s " % access_token)
+
+
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+	
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+	
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+	
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    print ("url sent for API access:%s"% url)
+    print ("API JSON result: %s" % result)
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
+
+# Helper functions
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
