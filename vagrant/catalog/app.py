8e00cf431a72576241d7b035bc7820045a12b3dc
Shardul Dave <daveshardul@rocketmail.com>
2019-04-23 22:08:29 -0700

    Oauth facebook login

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -61,11 +61,11 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    print ("access token received %s " % access_token)
+    #print("access token received %s " % access_token)
 
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
-	
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
@@ -73,35 +73,34 @@ def fbconnect():
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-	
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.8/me"
-    
+    #Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v3.2/me"
+
     token = result.split(',')[0].split(':')[1].replace('"', '')
-	
-    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+
+    url = 'https://graph.facebook.com/v3.2/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    print ("url sent for API access:%s"% url)
-    print ("API JSON result: %s" % result)
+    #print "url sent for API access:%s"% url
+    #print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
-    # The token must be stored in the login_session in order to properly logout
+    #The token must be stored in the login_session in order to properly logout
     login_session['access_token'] = token
 
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    #Get user picture
+    url = 'https://graph.facebook.com/v3.2/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
 
     login_session['picture'] = data["data"]["url"]
 
-    # see if user exists
+    #see if user exists
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
@@ -119,7 +118,17 @@ def fbconnect():
     flash("Now logged in as %s" % login_session['username'])
     return output
 
-# Helper functions
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"
+
+#Helper functions
 
 def createUser(login_session):
     newUser = User(name=login_session['username'], email=login_session[
@@ -141,6 +150,31 @@ def getUserID(email):
         return user.id
     except:
         return None
+
+@app.route('/')
+@app.route('/categories/')
+def showCategories():
+    categories = session.query(Category).all()
+    return render_template('index.html', categories=categories)
+
+
+# Disconnect based on provider
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showCategories'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showCategories'))
 
 
 if __name__ == '__main__':
