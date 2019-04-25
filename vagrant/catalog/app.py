1dcfebbba452e7de6f1540e9d70446ad0d59b253
Shardul Dave <daveshardul@rocketmail.com>
2019-04-25 13:18:23 -0700

    Stable versio 2.0

--- vagrant/catalog/app.py
+++ vagrant/catalog/app.py
@@ -53,10 +53,11 @@ def categoryJSON():
 # Oauth login for facebook
 
 
+# Create anti-forgery state token
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                    for x in range(32))
+                    for x in xrange(32))
     login_session['state'] = state
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
@@ -69,26 +70,35 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
-    # print("access token received %s " % access_token)
+    print "access token received %s " % access_token
+
+
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = """https://graph.facebook.com/oauth/access_token?
-            grant_type=fb_exchange_token&client_id=%s&client_secret=%s
-            &fb_exchange_token=%s""" % (app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
+
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v3.2/me"
-
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
     token = result.split(',')[0].split(':')[1].replace('"', '')
 
-    url = """https://graph.facebook.com/v3.2/me?
-            access_token=%s&fields=name,id,email""" % token
+    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
@@ -99,8 +109,7 @@ def fbconnect():
     login_session['access_token'] = token
 
     # Get user picture
-    url = """https://graph.facebook.com/v3.2/me/picture?
-            access_token=%s&redirect=0&height=200&width=200""" % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -120,8 +129,7 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += """ " style = "width: 300px; height: 300px;border-radius: 150px;
-                -webkit-border-radius: 150px;-moz-border-radius: 150px;"> """
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
 
     flash("Now logged in as %s" % login_session['username'])
     return output
@@ -132,8 +140,7 @@ def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
-    url = """https://graph.facebook.com/%s/permissions?
-            access_token=%s""" % (facebook_id, access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
