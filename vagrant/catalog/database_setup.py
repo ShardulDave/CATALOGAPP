d4016939d14097322b4b105794896ce0f929e7c5
Shardul Dave <daveshardul@rocketmail.com>
2019-04-27 10:35:32 -0700

    Added cascading

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -25,7 +25,7 @@ class Category(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship(User)
+    user = relationship(User, cascade="all, delete")
 
     @property
     def serialize(self):
@@ -43,8 +43,8 @@ class Item(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     user_id = Column(Integer, ForeignKey('users.id'))
-    category = relationship(Category)
-    user = relationship(User)
+    category = relationship(Category, cascade="all, delete")
+    user = relationship(User, cascade="all, delete")
 
     @property
     def serialize(self):
